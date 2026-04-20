68bba3f20ff24ce412e6f4a7a6c9223f490f470a
yasojan <yasinzahir98@gmail.com>
2026-04-19 21:08:05 -0700

    Finish menu and add pass by reference for vector methods

--- Phase I: Foundations/main.cpp
+++ Phase I: Foundations/main.cpp
@@ -13,11 +13,12 @@ Start small. Create a Part class with these fields: a part number (string, like 
 */
 
 #include <iostream>
-#include <iomanip>
+#include <vector>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::vector;
 
 class Part{
   private:
@@ -26,27 +27,74 @@ class Part{
     int quantity;
     double unit_cost;
   public:
-    Part(string part_num, string desc, int quantity, double unit_cost){
-      this->part_num = part_num;
-      this->desc = desc;
-      this->quantity = quantity;
-      this->unit_cost = unit_cost;
-    }
+    Part(string part_num, string desc, int quantity, double unit_cost) : part_num(part_num), desc(desc), quantity(quantity), unit_cost(unit_cost) {}
     void print(){
-      cout << "Part Number: " << this->part_num << endl;
-      cout << "Description: " << this->desc << endl;
-      cout << "Quantity: " << this->quantity << endl;
-      cout << "Unit cost: $" << this->unit_cost << endl;
+      cout << "Part Number: " << part_num << endl;
+      cout << "Description: " << desc << endl;
+      cout << "Quantity: " << quantity << endl;
+      cout << "Unit cost: $" << unit_cost << endl;
     }
 };
 
 // Write a main() that creates a Part on the stack, prints its fields to the console, and exits. Compile and run it. Commit.
+void list_items(vector<Part> &parts){
+  for(int i =0; i<parts.size(); i++){
+   parts[i].print();
+  }
+}
+void initialize_arr(vector<Part> &parts){
+  parts.push_back(Part("1", "Basic Mission: F-Fighter", 72, 999999.0));
+  parts.push_back(Part("2", "Basic Missions: F-Fighter & A-Attack", 585, 999999.0));
+  parts.push_back(Part("3", "Basic Mission: E-Special Electronic Installation", 585, 999999.0));
+}
+void add_part(vector<Part> &parts){
+  string part_num;
+  string desc;
+  int quantity;
+  double unit_cost;
 
+  cout << "Enter Part Number: #";
+  cin >> part_num;  
+  cin.ignore();
+
+  
+  cout << "Enter Description: ";
+  getline(cin, desc);
+
+  cout << "Enter quantity: ";
+  cin >> quantity;
+  cin.ignore();
+
+  cout << "Enter Unit Cost: $";
+  cin >> unit_cost;
+  cin.ignore();
+
+  parts.push_back(Part(part_num, desc, quantity, unit_cost));
+}
 int main(){
+  vector<Part> parts;
+  initialize_arr(parts);
+  int answer;
 
   cout << "-------------- C++ partstrack  --------------"<< endl;
-  Part growler("752823", "Carrier Based ECM/SEAD", 153, 999999.0 );
-  growler.print();
+  do {
+  cout << "-------------- Menu -------------- " << endl;
+  cout << "1. List all parts" << endl;
+  cout << "2. Add a part" << endl;
+  cout << "3. Quit" << endl;
+  cout << "-------------- Menu -------------- " << endl;
+  
+  cout << "Select Option: ";
+  cin >> answer;
+  cin.ignore();
+
+  if(answer == 1){
+    list_items(parts);
+  }
+  else if(answer == 2){
+    add_part(parts);
+  }
+  }while(answer != 3);
   cout << "-------------- C++ partstrack  --------------"<< endl;
 
   return 0;
